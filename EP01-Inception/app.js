1d36c435eae3992290bd95769ba9143cd8d38616
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-02 11:18:06 +0530

    sibling messy child

--- EP01-Inception/app.js
+++ EP01-Inception/app.js
@@ -1,11 +1,16 @@
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: 'child' }, [React.createElement('h1', {}, "i am child tag"),
-    React.createElement('h2',{}, "i am sibling")
-]))
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am child tag"),
+    React.createElement("h2", {}, "i am sibling"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am child tag1"),
+    React.createElement("h2", {}, "i am sibling2"),
+  ]),
+]);
 
 console.log(parent);
-
 
 // const heading = React.createElement("h1", { id: "abc" }, "hello world form react ");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-        
