52d05f91e6379119e5dc7752e171d2202a0afde9
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-02 11:09:54 +0530

    nested child

---
+++ EP01-Inception/app.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:'child'}, React.createElement('h1', {},"i am child tag")))
+
+console.log(parent);
+
+
+// const heading = React.createElement("h1", { id: "abc" }, "hello world form react ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+        
