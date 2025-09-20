e72cb8b712b5638f654d71b0c73ef2d35ee39409
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-20 13:02:02 +0530

    normal

--- EP03-FOUNDATION/app.js
+++ EP03-FOUNDATION/app.js
@@ -1,2 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
+
+//ELement react.createElement  ==> object ==> HTMLElemt(render)
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Labbaik ya rasool allah");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
